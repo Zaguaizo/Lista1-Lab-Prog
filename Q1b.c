33877469834ef4d57e9e7829a8f968ba263cbd07
Zaguaizo <104742084+Zaguaizo@users.noreply.github.com>
2024-03-15 10:23:55 -0300

    Create Q1b.c

---
+++ Q1b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  
+    float x;
+    
+    //A ordem das operações desta equação é : 1º %, 2º *, 3º /, 4º +, 5º -//
+    x = 2 % 2 + 2 * 2 - 2 / 2;
+    
+    printf("%f", x);
+    
+    
+    return 0;
+    
+}
