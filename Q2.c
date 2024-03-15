90bbae736ac207a705aabce79fd50b6c4f79898b
Zaguaizo <104742084+Zaguaizo@users.noreply.github.com>
2024-03-15 10:29:58 -0300

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+    int x;
+
+    // O inteiro
+    printf("Insira um decimal aqui: ");
+    
+    
+    scanf("%d", &x);
+
+    // Hexadecimal
+    printf("Valor convertido em Hexadecimal: %X\n", x);
+
+    // Octal
+    printf("Valor convertido em Octal: %o\n", x);
+
+
+    return 0;
+}
