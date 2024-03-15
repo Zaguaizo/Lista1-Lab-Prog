ad402f18c820bdea2a4cb2c3d589daa3e5c11880
Zaguaizo <104742084+Zaguaizo@users.noreply.github.com>
2024-03-15 10:39:27 -0300

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+    float x;
+
+    
+    printf("Qual o preço da conta: ");
+    
+    
+    scanf("%f", &x);
+
+   
+    printf("O valor total ficou: %f\n", x + (x/10));
+    
+
+
+    return 0;
+}
