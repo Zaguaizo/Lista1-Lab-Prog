1423396e7110f9bc316126ba216ee5cc3c5b046a
Zaguaizo <104742084+Zaguaizo@users.noreply.github.com>
2024-03-15 12:05:21 -0300

    Create Q8.c

---
+++ Q8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    
+    printf("Escolha um número inteiro: ");
+    scanf("%d", &x);
+    printf("O quadrado do número escolhido é: %d\n", x*x);
+    
+    return 0;
+}
