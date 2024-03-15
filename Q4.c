58b14c1eda358bb8127f621674dc07fcab6b22ce
Zaguaizo <104742084+Zaguaizo@users.noreply.github.com>
2024-03-15 10:37:51 -0300

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+    int x;
+
+    
+    printf("Insira um número inteiro aqui: ");
+    
+    
+    scanf("%d", &x);
+
+   
+    printf("Triplo: %d\n", 3 * x);
+    printf("Quadrado: %d\n", x * x);
+    printf("Meio: %d\n", x/2 );
+
+
+    return 0;
+}
