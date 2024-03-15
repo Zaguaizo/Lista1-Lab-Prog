789fedc5f3c1692ce63aa388e19c3a2de0fff544
Zaguaizo <104742084+Zaguaizo@users.noreply.github.com>
2024-03-15 10:34:31 -0300

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+    float x;
+
+    
+    printf("Insira um número real aqui: ");
+    
+    
+    scanf("%f", &x);
+
+   
+    printf("Numero inteiro do real: %.1f", x);
+
+
+    return 0;
+}
